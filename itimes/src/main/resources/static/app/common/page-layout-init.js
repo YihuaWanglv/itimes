82b6aae31301ffd1ecfdc79cf1f6f120a448b15e
yihuawanglv <wanglvyihua@gmail.com>
2016-04-14 16:15:02 +0800

    update bootstrap

--- itimes/src/main/resources/static/app/common/page-layout-init.js
+++ itimes/src/main/resources/static/app/common/page-layout-init.js
@@ -1,12 +1,12 @@
 $(function() {
   $("#header").load("./view/header.html", function(){
-    $(".menu-nav-right").on('click', function() {
-      if($('.nav-right').hasClass('my-navbar-right-show')) {
-        $(".nav-right").removeClass('my-navbar-right-show');
-      } else {
-        $('.nav-right').addClass('my-navbar-right-show');
-      }
-    });
+    // $(".menu-nav-right").on('click', function() {
+    //   if($('.nav-right').hasClass('my-navbar-right-show')) {
+    //     $(".nav-right").removeClass('my-navbar-right-show');
+    //   } else {
+    //     $('.nav-right').addClass('my-navbar-right-show');
+    //   }
+    // });
     if (TI_cookie.getCookie('username')) {
       $('.field-username').text(TI_cookie.getCookie('username'));
       $('.login-on').show();
@@ -16,5 +16,5 @@ $(function() {
       $('.login-on').hide();
     }
   });
-  $(".nav-right-holder").load("./view/nav-right.html");
+  // $(".nav-right-holder").load("./view/nav-right.html");
 });
